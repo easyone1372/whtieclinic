db5a2f3907d1de3d13e5da30e692fdc5edb49d77
yonghyuny <lovelove4695@naver.com>
2024-11-14 15:43:02 +0900

    241114용현 기사님목록 데이터불러오기 성공

--- src/components/molecules/FooterDisplay/DataDisplay.tsx
+++ src/components/molecules/FooterDisplay/DataDisplay.tsx
@@ -13,20 +13,22 @@ const DataDisplay = <T extends Record<string, any>>({
 }: DataDisplayProps<T>) => (
   <>
     {Object.entries(data)
-      .filter(([key]) => labelMap[key as keyof T]) // labelMap에 없는 키는 건너뛰기
+      .filter(([key]) => labelMap[key as keyof T]) // labelMap에 없는 키는 건너뛰기(id(고유번호)해당)
       .map(([key, value]) => (
         <div key={key} className="flex gap-1">
           {/* 데이터 키에 대한 레이블 (커스텀 레이블 또는 기본 키 사용) */}
           <ShaText text={`${labelMap[key as keyof T] || key}:`} isBold size="small" />
 
-          {/* 데이터 값 표시: 배열이면 콤마로 연결하여 표시, 'commission' 키면 퍼센트 추가 */}
+          {/* 데이터 값 표시: 배열이면 콤마로 연결하여 표시, 'commission' 키면 퍼센트 추가 휴가일 없으면 없음이라고 띄우기 */}
           <ShaText
             text={
-              key === 'engineerCommissionRate'
-                ? `${value}%`
-                : Array.isArray(value)
-                  ? value.join(', ')
-                  : value
+              value === null || value === undefined
+                ? '없음'
+                : key === 'engineerCommissionRate'
+                  ? `${value}%`
+                  : Array.isArray(value)
+                    ? value.join(', ')
+                    : value
             }
             size="small"
           />
